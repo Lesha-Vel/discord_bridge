65b1384887ec3d0815b495accb1797b3bf55369f
Lesha_Vel <mail@example.com>
2026-02-05 19:35:10 +1000

    mod settings names now starts with actual mod name
    also added missing host setting

--- init.lua
+++ init.lua
@@ -1,60 +1,60 @@
 local http = minetest.request_http_api()
 local settings = minetest.settings
 
-local host = settings:get('discord.host') or 'localhost'
-local port = settings:get('discord.port') or 8080
-local escape_formatting = settings:get_bool('discord.escape_formatting', false)
+local host = settings:get('discord_bridge.host') or 'localhost'
+local port = settings:get('discord_bridge.port') or 8080
+local escape_formatting = settings:get_bool('discord_bridge.escape_formatting', false)
 local timeout = 10
 
 local discord_bridge = {}
 discord = {}
 
 -- Configuration
-discord_bridge.text_colorization = settings:get('discord.text_color') or '#ffffff'
+discord_bridge.text_colorization = settings:get('discord_bridge.text_color') or '#ffffff'
 
-discord_bridge.clean_invites = settings:get_bool('discord.clean_invites', true)
-discord_bridge.date = settings:get('discord.date') or '%d.%m.%Y %H:%M'
+discord_bridge.clean_invites = settings:get_bool('discord_bridge.clean_invites', true)
+discord_bridge.date = settings:get('discord_bridge.date') or '%d.%m.%Y %H:%M'
 
-discord_bridge.send_server_startup = settings:get_bool('discord.send_server_startup', true)
-discord_bridge.send_server_shutdown = settings:get_bool('discord.send_server_shutdown', true)
-discord_bridge.include_server_status_on_startup = settings:get_bool('discord.include_server_status_on_startup', true)
-discord_bridge.include_server_status_on_shutdown = settings:get_bool('discord.include_server_status_on_shutdown', true)
-discord_bridge.send_joins = settings:get_bool('discord.send_joins', true)
-discord_bridge.send_last_login = settings:get_bool('discord.send_last_login', false)
-discord_bridge.send_leaves = settings:get_bool('discord.send_leaves', true)
-discord_bridge.send_welcomes = settings:get_bool('discord.send_welcomes', true)
-discord_bridge.send_deaths = settings:get_bool('discord.send_deaths', true)
+discord_bridge.send_server_startup = settings:get_bool('discord_bridge.send_server_startup', true)
+discord_bridge.send_server_shutdown = settings:get_bool('discord_bridge.send_server_shutdown', true)
+discord_bridge.include_server_status_on_startup = settings:get_bool('discord_bridge.include_server_status_on_startup', true)
+discord_bridge.include_server_status_on_shutdown = settings:get_bool('discord_bridge.include_server_status_on_shutdown', true)
+discord_bridge.send_joins = settings:get_bool('discord_bridge.send_joins', true)
+discord_bridge.send_last_login = settings:get_bool('discord_bridge.send_last_login', false)
+discord_bridge.send_leaves = settings:get_bool('discord_bridge.send_leaves', true)
+discord_bridge.send_welcomes = settings:get_bool('discord_bridge.send_welcomes', true)
+discord_bridge.send_deaths = settings:get_bool('discord_bridge.send_deaths', true)
 
-discord_bridge.name_wrapper = settings:get('discord.name_wrapper') or '<**@1**>  '
-discord_bridge.startup_text = settings:get('discord.startup_text') or '*** Server started!'
-discord_bridge.shutdown_text = settings:get('discord.shutdown_text') or '*** Server shutting down...'
-discord_bridge.join_text = settings:get('discord.join_text') or '\\*\\*\\* **@1** joined the game'
-discord_bridge.last_login_text = settings:get('discord.last_login_text') or '\\*\\*\\* **@1** joined the game. Last login: @2'
-discord_bridge.leave_text = settings:get('discord.leave_text') or '\\*\\*\\* **@1** left the game'
-discord_bridge.welcome_text = settings:get('discord.welcome_text') or '\\*\\*\\* **@1** joined the game for the first time. Welcome!'
-discord_bridge.death_text = settings:get('discord.death_text') or '\\*\\*\\* **@1** died'
-discord_bridge.chat_message_format = settings:get('discord.chat_message_format') or '<%s@Discord> %s'
+discord_bridge.name_wrapper = settings:get('discord_bridge.name_wrapper') or '<**@1**>  '
+discord_bridge.startup_text = settings:get('discord_bridge.startup_text') or '*** Server started!'
+discord_bridge.shutdown_text = settings:get('discord_bridge.shutdown_text') or '*** Server shutting down...'
+discord_bridge.join_text = settings:get('discord_bridge.join_text') or '\\*\\*\\* **@1** joined the game'
+discord_bridge.last_login_text = settings:get('discord_bridge.last_login_text') or '\\*\\*\\* **@1** joined the game. Last login: @2'
+discord_bridge.leave_text = settings:get('discord_bridge.leave_text') or '\\*\\*\\* **@1** left the game'
+discord_bridge.welcome_text = settings:get('discord_bridge.welcome_text') or '\\*\\*\\* **@1** joined the game for the first time. Welcome!'
+discord_bridge.death_text = settings:get('discord_bridge.death_text') or '\\*\\*\\* **@1** died'
+discord_bridge.chat_message_format = settings:get('discord_bridge.chat_message_format') or '<%s@Discord> %s'
 
-discord_bridge.use_embeds_on_joins_and_leaves = settings:get_bool('discord.use_embeds_on_joins_and_leaves', true)
-discord_bridge.use_embeds_on_welcomes = settings:get_bool('discord.use_embeds_on_welcomes', true)
-discord_bridge.use_embeds_on_deaths = settings:get_bool('discord.use_embeds_on_deaths', true)
-discord_bridge.use_embeds_on_server_updates = settings:get_bool('discord.use_embeds_on_server_updates', true)
-discord_bridge.use_embeds_on_dm_cmd = settings:get_bool('discord.use_embeds_on_dm_cmd', false)
-discord_bridge.use_embeds_on_svc_dms = settings:get_bool('discord.use_embeds_on_svc_dms', false)
+discord_bridge.use_embeds_on_joins_and_leaves = settings:get_bool('discord_bridge.use_embeds_on_joins_and_leaves', true)
+discord_bridge.use_embeds_on_welcomes = settings:get_bool('discord_bridge.use_embeds_on_welcomes', true)
+discord_bridge.use_embeds_on_deaths = settings:get_bool('discord_bridge.use_embeds_on_deaths', true)
+discord_bridge.use_embeds_on_server_updates = settings:get_bool('discord_bridge.use_embeds_on_server_updates', true)
+discord_bridge.use_embeds_on_dm_cmd = settings:get_bool('discord_bridge.use_embeds_on_dm_cmd', false)
+discord_bridge.use_embeds_on_svc_dms = settings:get_bool('discord_bridge.use_embeds_on_svc_dms', false)
 
-discord_bridge.startup_color = settings:get('discord.startup_color') or '#5865f2'
-discord_bridge.shutdown_color = settings:get('discord.shutdown_color') or 'NOT_SET'
-discord_bridge.join_color = settings:get('discord.join_color') or '#57f287'
-discord_bridge.leave_color = settings:get('discord.leave_color') or '#ed4245'
-discord_bridge.welcome_color = settings:get('discord.welcome_color') or '#57f287'
-discord_bridge.death_color = settings:get('discord.death_color') or 'NOT_SET'
-discord_bridge.cmd_chat_send_player_color = settings:get('discord.cmd_chat_send_player_color') or 'NOT_SET'
-discord_bridge.cmd_ret_value_color = settings:get('discord.cmd_ret_value_color') or 'NOT_SET'
-discord_bridge.svc_dms_banned_color = settings:get('discord.svc_dms_banned_color') or '#ed4245'
-discord_bridge.svc_dms_privs_color = settings:get('discord.svc_dms_privs_color') or '#ede442'
-discord_bridge.svc_dms_cnf_color = settings:get('discord.svc_dms_cnf_color') or '#ed9d42'
-discord_bridge.login_success_color = settings:get('discord.login_success_color') or '#57f287'
-discord_bridge.login_fail_color = settings:get('discord.login_fail_color') or '#ed4245'
+discord_bridge.startup_color = settings:get('discord_bridge.startup_color') or '#5865f2'
+discord_bridge.shutdown_color = settings:get('discord_bridge.shutdown_color') or 'NOT_SET'
+discord_bridge.join_color = settings:get('discord_bridge.join_color') or '#57f287'
+discord_bridge.leave_color = settings:get('discord_bridge.leave_color') or '#ed4245'
+discord_bridge.welcome_color = settings:get('discord_bridge.welcome_color') or '#57f287'
+discord_bridge.death_color = settings:get('discord_bridge.death_color') or 'NOT_SET'
+discord_bridge.cmd_chat_send_player_color = settings:get('discord_bridge.cmd_chat_send_player_color') or 'NOT_SET'
+discord_bridge.cmd_ret_value_color = settings:get('discord_bridge.cmd_ret_value_color') or 'NOT_SET'
+discord_bridge.svc_dms_banned_color = settings:get('discord_bridge.svc_dms_banned_color') or '#ed4245'
+discord_bridge.svc_dms_privs_color = settings:get('discord_bridge.svc_dms_privs_color') or '#ede442'
+discord_bridge.svc_dms_cnf_color = settings:get('discord_bridge.svc_dms_cnf_color') or '#ed9d42'
+discord_bridge.login_success_color = settings:get('discord_bridge.login_success_color') or '#57f287'
+discord_bridge.login_fail_color = settings:get('discord_bridge.login_fail_color') or '#ed4245'
 
 discord_bridge.registered_on_messages = {}
 
