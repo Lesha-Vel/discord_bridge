92a7121049d78b74e3b8c7a9a7b6f6d103536ae7
Lesha_Vel <mail@example.com>
2026-02-13 16:50:13 +1000

    added `import_player_pos.py` script

---
+++ utils/import_player_pos.py
@@ -0,0 +1,15 @@
+#arguments: <world dir>
+import sys
+import sqlite3
+
+with sqlite3.connect(sys.argv[1] + '/players.sqlite') as players:
+    with sqlite3.connect(sys.argv[1] + '/mod_storage.sqlite') as mod_storage:
+        playersCur = players.cursor()
+        msCur = mod_storage.cursor()
+        playersCur.execute("SELECT name,posX,posY,posZ FROM player;")
+        while True:
+            entry = playersCur.fetchone()
+            if not entry:
+                break
+            msCur.execute("INSERT OR REPLACE INTO entries VALUES ('discord_bridge',?,?)", [sqlite3.Binary(('_' + entry[0]).encode('utf-8')),
+                sqlite3.Binary(('(' + str(entry[1]) + ',' + str(entry[2]) + ',' + str(entry[3]) + ')').encode('utf-8'))])
