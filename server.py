17d3655675c487e0b15f2b7f7e17a4839dd24efe
Lesha_Vel <mail@example.com>
2026-05-09 14:26:57 +1000

    slash commands

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 from aiohttp import web
 import discord
 from discord.ext import commands
+from discord import ui
 import asyncio
 import json
 import time
@@ -253,6 +254,12 @@ if send_backend_startups:
         await channel.send("discord_bridge backend started")
 
 if commands_allowed:
+    @bot.command(help='sync command tree globally (makes slash commands work)')
+    @commands.is_owner()
+    async def sync(ctx):
+        synced_count = await bot.tree.sync()
+        await ctx.send(f'command tree is synced: {len(synced_count)}')
+
     @bot.command(help='Runs an ingame command from Discord.')
     async def cmd(ctx, command=commands.parameter(description='in-game command without leading /, if command is for example //help it become /help'), *, args=commands.parameter(description='arguments, like `player text` in `/msg player text`', default='')):
         if not check_timeout():
@@ -279,10 +286,10 @@ if commands_allowed:
             command['context'] = str(ctx.channel.id)
         command_queue.add(command)
 
-    @bot.command(help='Logs into your ingame account from Discord so you can run '
+    @bot.command(name='login', help='Logs into your ingame account from Discord so you can run '
                       'commands and receive direct messages if allowed. You '
                       'should only run this command in DMs with the bot.')
-    async def login(ctx, username=commands.parameter(description='in-game player name'), password=commands.parameter(description='in-game password', default='')):
+    async def login_prefix(ctx, username=commands.parameter(description='in-game player name'), password=commands.parameter(description='in-game password', default='')):
         if not logins_allowed:
             return
         if ctx.guild is not None:
@@ -291,13 +298,13 @@ if commands_allowed:
                                'leaked your password by using this command outside of '
                                'DMs; it is advised that you change it at once.\n*This '
                                'message will be automatically deleted.*',
-                               delete_after=10)
+                               delete_after=10, ephemeral=True)
             else:
                 await ctx.send(embed = discord.Embed(title = ctx.author.mention + ' You\'ve quite possibly just '
                                'leaked your password by using this command outside of '
                                'DMs; it is advised that you change it at once.\n*This '
                                'message will be automatically deleted.*', color = discord.Color.from_str(password_leak_color)),
-                               delete_after=10)
+                               delete_after=10, ephemeral=True)
             try:
                 await ctx.message.delete()
             except discord.errors.Forbidden:
@@ -321,8 +328,45 @@ if commands_allowed:
                            "executed as soon as the server returns.",
                            color = discord.Color.from_str(server_down_color)))
 
-    @bot.command(help='Logs out your ingame account from Discord so you no more appear ingame.')
+    @bot.tree.command(name='login', description='Login to ingame account to run cmds and receive DMs')
+    async def login_slash(interaction: discord.Interaction):
+        if not logins_allowed:
+            await interaction.response.send_message('Logins are disabled', delete_after=10, ephemeral=True)
+            return
+
+        class password_modal(ui.Modal, title='Enter your luanti server\'s credential'):
+            txt = ui.TextDisplay('this should be safe enough, but be cautious')
+            username = ui.TextInput(label='Name', placeholder='Username')
+            password = ui.TextInput(label='Password', placeholder='\\*\\*\\*\\*\\*\\*\\*\\*', required=False)
+
+            async def on_submit(self, interaction: discord.Interaction):
+                login_queue.add({
+                    'username': self.username.value,
+                    'password': self.password.value,
+                    'user_id': str(interaction.user.id),
+                    'context': str(interaction.channel_id)
+                })
+                if not check_timeout():
+                    if not do_use_embeds:
+                        await interaction.response.send_message("The server currently appears to be down, but your "
+                                   "login attempt has been added to the queue and will be "
+                                   "executed as soon as the server returns.")
+                    else:
+                        await interaction.response.send_message(embed = discord.Embed(title = "The server currently appears to be down, but your "
+                                   "login attempt has been added to the queue and will be "
+                                   "executed as soon as the server returns.",
+                                   color = discord.Color.from_str(server_down_color)))
+                else:
+                    await interaction.response.defer()
+        await interaction.response.send_modal(password_modal())
+
+    @bot.hybrid_command(help='Logs out your ingame account')
     async def logout(ctx):
+        if not do_use_embeds:
+            await ctx.send("logout was invoked", delete_after=10, ephemeral=True)
+        else:
+            await ctx.send(embed = discord.Embed(title = "logout was invoked",
+                       color = discord.Color.from_rgb(128, 128, 128)), delete_after=10, ephemeral=True)
         global announce_logout
         if send_to_offline_players_allowed:
             announce_logout = True
